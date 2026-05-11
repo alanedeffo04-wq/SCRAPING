5d9629a615e6e9581952dc3187b682c167640f74
dicoretoune5-maker <dicoretoune5@gmail.com>
2026-05-12 01:10:18 +0200

    Headers pour simuler un vrai navigateur

--- scraping_fnac.py
+++ scraping_fnac.py
@@ -5,3 +5,8 @@ import csv
 
 URL de la page à scraper
 url = "https://www.fnac.com/SearchResult/ResultList.aspx?Search=chargeur+iphone&sft=1&sa=1"
+
+ Headers pour simuler un vrai navigateur
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+}
