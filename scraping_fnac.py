7d5d814190c118ad9da5312899e7381d4cafd0a8
dicoretoune5-maker <dicoretoune5@gmail.com>
2026-05-12 01:47:02 +0200

    export en csv

--- scraping_fnac.py
+++ scraping_fnac.py
@@ -3,19 +3,19 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-URL de la page à scraper
+ # URL de la page à scraper # type: ignore
 url = "https://www.fnac.com/SearchResult/ResultList.aspx?Search=chargeur+iphone&sft=1&sa=1"
 
- Headers pour simuler un vrai navigateur
+ #Headers pour simuler un vrai navigateur
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 }
 response = requests.get(url, headers=headers)
 
-Vérification
+ #Vérification
 if response.status_code == 200:
 
-    # 5. Analyse du HTML
+    #  Analyse du HTML
     soup = BeautifulSoup(response.text, "lxml")
 
     # 6. Récupération des blocs produits
@@ -23,7 +23,7 @@ if response.status_code == 200:
 
     resultats = []
 
- Extraction des données
+    # Extraction des données
     for produit in produits:
         nom = produit.find("h2")
         prix = produit.find("span", class_="Article-price")
@@ -33,3 +33,14 @@ if response.status_code == 200:
                 "nom_produit": nom.get_text(strip=True),
                 "prix": prix.get_text(strip=True)
             })
+
+    # Export en CSV
+    with open("fnac_chargeur_iphone.csv", "w", newline="", encoding="utf-8") as fichier:
+        writer = csv.DictWriter(fichier, fieldnames=["nom_produit", "prix"])
+        writer.writeheader()
+        writer.writerows(resultats)
+
+    print(" Scraping terminé. Fichier créé.")
+
+else:
+    print(" Erreur HTTP :", response.status_code)
