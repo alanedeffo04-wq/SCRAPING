493fc068326aa56e541cc3f645a7824f1b502f3f
dicoretoune5-maker <dicoretoune5@gmail.com>
2026-05-12 01:15:19 +0200

    une verification et analyse du html après  récupération des blocs produits

--- scraping_fnac.py
+++ scraping_fnac.py
@@ -11,3 +11,14 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 }
 response = requests.get(url, headers=headers)
+
+Vérification
+if response.status_code == 200:
+
+    # 5. Analyse du HTML
+    soup = BeautifulSoup(response.text, "lxml")
+
+    # 6. Récupération des blocs produits
+    produits = soup.find_all("article")
+
+    resultats = []
